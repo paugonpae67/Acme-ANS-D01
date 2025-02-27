c19044384263dc9dc014013a27f8601e7432e4e6
paula <paulagonzalezllanes@gmail.com>
2025-02-27 09:54:13 +0100

    feat: Entidad Technician creada
    Se ha creado la entidad technician

---
+++ src/main/java/acme/entities/technicians/Technician.java
@@ -0,0 +1,51 @@
+
+package acme.entities.technicians;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Technician extends AbstractEntity {
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String	licenseNumber;
+
+	@Mandatory
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String	phoneNumber;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String	specialisation;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean	healthTestPassed;
+
+	@Mandatory
+	@ValidNumber
+	@Automapped
+	private Integer	yearsOfexperience;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String	certifications;
+}
